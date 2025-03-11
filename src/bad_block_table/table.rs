e47ce8163e0229acb0cfb861e922aa2631fa2159
birkj <jens.birk.andersen@gmail.com>
2025-03-11 11:42:06 +0100

    add init for bad block

--- src/bad_block_table/table.rs
+++ src/bad_block_table/table.rs
@@ -7,59 +7,46 @@ struct BadBlockTable<'c> {
 struct Channel<'pu> {
     parallel_units: &'pu mut [ParallelUnit<'pu>],
     n_parallel_units: usize,
-    id: usize
+    id: usize,
 }
 
 struct ParallelUnit<'c> {
     chunks: &'c mut [Chunk<'c>],
     n_chunks: usize,
-    id: usize
+    id: usize,
 }
 
 struct Chunk<'pb> {
     plane_blocks: &'pb mut [PlaneBlock],
     n_plane_blocks: usize,
-    id: usize
+    id: usize,
 }
 
 struct PlaneBlock {
     n_planes: u8,
-    id: usize
+    id: usize,
 }
 
-impl BadBlockTable<'_> {
-    fn init() -> Self {
-        todo!()
-    }
-}
-
-impl Channel<'_> {
-    fn init() -> Self {
-        todo!()
-    }
-}
-
-impl ParallelUnit<'_> {
-    fn init() -> Self {
-        todo!()
-    }
-}
-
-impl Chunk<'_> {
-    fn check_bad_blocks(self) -> Self {
-        // for plane_block in
-        todo!()
+impl<'c> BadBlockTable<'c> {
+    fn init(&mut self) {
+        for channel in &mut *self.channels {
+            for pu in &mut *channel.parallel_units {
+                for chunk in &mut *pu.chunks {
+                    for plane_block in &mut *chunk.plane_blocks {
+                        let is_bad = plane_block.health_check();
+                        if is_bad {
+                            // Mark as bad somehow
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 impl PlaneBlock {
-    fn check_if_bad(self, channel_id, lun_id, block_id) -> bool {
-        let page = 0;
-        // channel
-        // lun
-        // block
-        // 
-
-        todo!()
+    fn health_check(&self) -> bool {
+        // do a call to the media manager
+        false
     }
 }
